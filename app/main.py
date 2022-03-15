39175ad55f1585998bdcfbf133df040a8a0e8687
DevarshSShah <101546501+DevarshSShah@users.noreply.github.com>
2022-03-15 15:48:56 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -47,7 +47,7 @@ if not sta.isconnected():
         pass
 print('Connected to WiFi')
 print('network config:', sta.ifconfig())
-print('Your code has been updated succesfully')
+print('Your code has been updated succesfully Test successfull')
 
 #Connecting to the mqtt broker
 client = MQTTClient(mac_address, CONFIG['MQTT_BROKER'], user=CONFIG['USER'], password=CONFIG['PASSWORD'], port=CONFIG['PORT'])
